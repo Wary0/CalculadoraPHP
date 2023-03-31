5e6ee3527a6dce4ae630cc19abe085fd3c2e1634
Wary0 <clayton.silva124@etec.sp.gov.br>
2023-03-30 22:00:19 -0300

    Calc

---
+++ src/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="estilo.css">
+    <meta charset="UTF-8">
+    <title>Calculadora</title>
+</head>
+<body>
+    <form method="post" action="main.php">
+        <label for="num1">Numero 1:</label>
+        <input type="text" name="num1" id="num1">
+        <label for="num1">Numero 2:</label>
+        <input type="text" name="num2" id="num2">
+        <label for="op">Operação</label>
+        <select name="op" id="op">
+            <option value="+">somar</option>
+            <option value="-">multiplicar</option>
+            <option value="*">subtrair</option>
+            <option value="/">dividir</option>
+        </select>
+        <br>
+        <button type="submit">Calcular</button>
+</form>
+</body>
+</html>
